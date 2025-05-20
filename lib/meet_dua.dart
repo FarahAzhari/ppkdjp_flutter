f3247cd6c36f152fa76e5da1a83ee62b69ac7daa
farahazhari <116514965+FarahAzhari@users.noreply.github.com>
2025-05-20 10:26:52 +0700

    Add Tugas 1 Flutter

---
+++ lib/meet_dua.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MeetDua extends StatelessWidget {
+  const MeetDua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meet Dua'),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center),
+          SizedBox(height: 20),
+          Text('PPKD Batch 2'),
+          Text('Mobile Programming Class'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(children: [Text('We are in this together')]),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(children: [Text('We are in this together')]),
+          ),
+          Row(children: [Text('We are in this together')]),
+
+          Container(
+            // height: 40,
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.all(12),
+            width: double.infinity,
+            // color: Colors.black, //Jangan letakkan ini di luar BoxDecoration jika menggunakan BoxDecoration
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.all(Radius.circular(24)),
+              border: Border.all(color: Colors.blue),
+              // shape: BoxShape.circle
+              gradient: LinearGradient(
+                colors: [Colors.amber, Colors.redAccent],
+              ),
+            ),
+            child: Column(
+              children: [
+                Text('Data', style: TextStyle(color: Colors.white)),
+                Text('Data', style: TextStyle(color: Colors.white)),
+                Text('Data', style: TextStyle(color: Colors.white)),
+                Text('Data', style: TextStyle(color: Colors.white)),
+                Text('Data', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/tugas_satu_flutter.dart
+++ lib/tugas_satu_flutter.dart
@@ -61,9 +61,8 @@ class TugasSatuFlutter extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 30,
-                  backgroundImage: NetworkImage(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU62Md2yC4lIg8Mt_ZUGEVBaoXR7apfYbWbQ&s',
-                  ),
+                  child: Icon(Icons.person, color: Colors.white, size: 40),
+                  backgroundColor: Colors.black,
                 ),
                 SizedBox(width: 16),
                 Column(
@@ -89,6 +88,17 @@ class TugasSatuFlutter extends StatelessWidget {
                   ],
                 ),
               ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Im a Flutter Student. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', // Your text here
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
             ),
           ),
         ],

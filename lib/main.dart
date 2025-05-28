08019ede86fd996c9cd2f04627fe7d122eff220a
farahazhari <116514965+FarahAzhari@users.noreply.github.com>
2025-05-28 13:58:11 +0700

    Add: Tugas 8 Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:ppkdjp_flutter/meet_tujuh/tugas_tujuh_flutter.dart';
+import 'package:ppkdjp_flutter/meet_delapan/tugas_delapan_flutter.dart';
 // import 'package:ppkdjp_flutter/meet_dua.dart';
 // import 'package:ppkdjp_flutter/meet_dua_a.dart';
 // import 'package:ppkdjp_flutter/tugas_dua_flutter.dart';
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const TugasTujuhFlutter(),
+      home: TugasDelapanFlutter(),
     );
   }
 }
--- lib/meet_delapan/tugas_delapan_flutter.dart
+++ lib/meet_delapan/tugas_delapan_flutter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ppkdjp_flutter/constant/app_color.dart';
+import 'package:ppkdjp_flutter/meet_tujuh/tugas_tujuh_flutter.dart';
 
 class TugasDelapanFlutter extends StatefulWidget {
   const TugasDelapanFlutter({super.key});
@@ -8,8 +10,64 @@ class TugasDelapanFlutter extends StatefulWidget {
 }
 
 class _TugasDelapanFlutterState extends State<TugasDelapanFlutter> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const TugasTujuhFlutter(), // Halaman dengan Drawer & Form
+    const AboutPage(), // Halaman Tentang Aplikasi
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: AppColor.army1,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info_outline),
+            label: 'Tentang',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('Tentang Aplikasi'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Aplikasi Form Input',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Aplikasi ini dibuat untuk mengumpulkan data pengguna melalui form.',
+            ),
+            SizedBox(height: 20),
+            Text('Pembuat: Farah Azhari'),
+            Text('Versi: 1.0.0'),
+          ],
+        ),
+      ),
+    );
   }
 }
